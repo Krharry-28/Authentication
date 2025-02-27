cb35349909db782cfa902e7cee27cae0a51385ee
Harshvardhan <sinhaharshvardhan31@gmail.com>
2025-02-27 20:47:15 +0530

    initial commit

---
+++ backend/Routes/ProductRouter.js
@@ -0,0 +1,14 @@
+const ensureAuthenticated = require('../Middlewares/Auth');
+
+const router = require('express').Router();
+
+router.get('/', ensureAuthenticated, (req, res) => {
+    console.log('---- logged in user detail ---', req.user);
+    res.status(200).json([
+        {
+           "Welcome ": "Welcome to the dashboard",
+        }
+    ])
+});
+
+module.exports = router;
